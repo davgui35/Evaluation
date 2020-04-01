42e5d20d45fe11dcae9166653f87d0b6d3487ac5
davgui35 <50999028+davgui35@users.noreply.github.com>
2020-04-01 11:08:16 +0200

    Moitie du site

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
+    var width = $('#container').width();
+    console.log(width);//recupere la largeur
+    // la mettre aussi dans height
+    $('#container div').height(width);
+
+    $('#table')
+    	// Variables contenant l'ensemble des liens du menu par onglet :
+        var $ongletItems = $(".onglets li") ;
+    
+		
+		// Fonction d�clench�es quand on clique sur l'un de ces items
+		$ongletItems.click(function(){
+			
+			// On enl�ve la classe sur tous les items
+			$ongletItems.removeClass("active") ;
+			
+			// On met la classe active sur l'item qui a �t� cliqu�
+			$(this).addClass("active") ;
+			
+			// Annule l'action par d�faut
+			return false ;
+			
+        });
+        console.log($ongletItems);
+        // Je stocke mes sections et je les cache
+        //$sections = $("section").css("display","none") 
+        console.log($sections);
+		
    
+		
 });
